1e790e214cc94551a61c469c06a9d813b54c326e
DESKTOP-5SM091D\cemel <cemeloen@gmail.com>
2018-09-01 21:47:22 +0200

    GameManager Added

---
+++ GMTK Gamejam/Assets/Scripts/Round/Round.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class Round : MonoBehaviour
+{
+	private Timer _timer = new Timer();
+	public Timer Timer
+	{
+		get
+		{
+			return _timer;
+		}
+	}
+
+	#region C-Sharp Events
+	public delegate void RoundSetupHandler();
+	public event RoundSetupHandler RoundSetup;
+	public delegate void RoundStartHandler();
+	public event RoundStartHandler RoundStart;
+	#endregion
+
+	private void Awake()
+	{
+		_timer.Finished += OnSetupTimerFinished;
+	}
+
+	public void Starting()
+	{
+		if (RoundStart != null)
+			RoundStart();
+	}
+
+	public void Setup()
+	{
+		if (RoundSetup != null)
+			RoundSetup();
+	}
+
+	private void OnSetupTimerFinished()
+	{
+		Starting();
+	}
+}
---
+++ GMTK Gamejam/Assets/Scripts/Timer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer
+{
+	private int _currentSeconds;
+
+	#region C-Sharp Events
+	public delegate void TimerFinishHandler();
+	public event TimerFinishHandler Finished;
+	public delegate void TimerSecondsChangedHandler(int seconds);
+	public event TimerSecondsChangedHandler SecondsChanged;
+	#endregion
+
+	public void Start(int seconds)
+	{
+		_currentSeconds = seconds;
+		GameObject.FindObjectOfType<MonoBehaviour>().StartCoroutine(Routine(seconds));
+	}
+
+	private IEnumerator Routine(int seconds)
+	{
+		while (_currentSeconds > 0)
+		{
+			SecondsChanged(_currentSeconds);
+			yield return new WaitForSeconds(1);
+			_currentSeconds--;
+		}
+		SecondsChanged(_currentSeconds);
+		Finished();
+	}
+}
