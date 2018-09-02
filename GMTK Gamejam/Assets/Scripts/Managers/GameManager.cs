779a29be7eb44addae9d2c04f2856aea6ae807ce
DESKTOP-5SM091D\cemel <cemeloen@gmail.com>
2018-09-02 15:53:45 +0200

    Round Events and Round UI Remake

--- GMTK Gamejam/Assets/Scripts/Managers/GameManager.cs
+++ GMTK Gamejam/Assets/Scripts/Managers/GameManager.cs
@@ -59,5 +59,6 @@ public class GameManager : MonoBehaviour
 	private void OnRoundStart()
 	{
 		GameObject.FindObjectOfType<PlayerMovement>().enabled = true;
+		GameObject.FindObjectOfType<ColorChange>().enabled = true;
 	}
 }
