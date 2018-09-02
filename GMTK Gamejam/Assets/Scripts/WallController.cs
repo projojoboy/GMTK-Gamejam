5bbd8b318bd1914a4def1a1ec848893bcb7e700b
Projojoboy <22048@ma-web.nl>
2018-09-02 16:06:13 +0200

    MainMenu Update

--- GMTK Gamejam/Assets/Scripts/WallController.cs
+++ GMTK Gamejam/Assets/Scripts/WallController.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 public class WallController : MonoBehaviour {
 
     [Header("4 = Nothing, 0 = left, 1 = Right, 2 = Up, 3 = Down")]
-    [SerializeField] private int pushDir = 4; // 4 = Nothing, 0 = left, 1 = Right, 2 = Up, 3 = Down
+    public int pushDir = 4; // 4 = Nothing, 0 = left, 1 = Right, 2 = Up, 3 = Down
 
     PlayerMovement pm;
 
 	// Use this for initialization
 	void Start () {
-        pm = GameObject.Find("Player").GetComponent<PlayerMovement>();
+        if(GameObject.Find("Player"))
+            pm = GameObject.Find("Player").GetComponent<PlayerMovement>();
 	}
 
     private void OnCollisionEnter2D(Collision2D coll)
